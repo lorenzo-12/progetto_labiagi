6f95b25456ae2d83b0568028180bc0d8a50c1088
lorenzo-12 <lorenzodifilippo12@gmail.com>
2021-06-10 20:21:51 +0200

    aggiunto al server il modo per far muover il robot

---
+++ build/ped/CMakeFiles/ped_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ped_client.dir/src/ped_client.cpp.o"
+  "/home/me/progetto_labiagi/devel/.private/ped/lib/ped/ped_client.pdb"
+  "/home/me/progetto_labiagi/devel/.private/ped/lib/ped/ped_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ped_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/ped/src/ped_client.cpp
+++ src/ped/src/ped_client.cpp
@@ -1,12 +1,25 @@
 #include "utils.h"
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+
 using namespace std;
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "ped_client");
 	ros::NodeHandle n;
 	
-	ros::ServiceClient client = n.serviceClient<"nome_package"::"tipo_dato">("map");
+	ros::Publisher pub = n.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1000);
+	
+	geometry_msgs::PoseStamped goal;
+	goal.header.frame_id="map";
+	cout << "x: "; cin >> goal.pose.position.x;
+	cout << "y: "; cin >> goal.pose.position.y;
+	//cout << "time: "; cin >> goal.header.stamp.sec;
+	goal.header.stamp = ros::Time::now();
+	cout << goal.pose.position.x << " " << goal.pose.position.y << endl;
+	pub.publish(goal);
 	
 	ros::spin();
+	cout << "fine" << endl;
 	
 }
