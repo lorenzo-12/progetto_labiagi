68f01b013208bb3cb11aa2636e04ff0132330dd9
lorenzo-12 <lorenzodifilippo12@gmail.com>
2021-06-12 01:28:44 +0200

    aggiunta spiegazione alla fine del client

--- src/ped/src/ped_client.cpp
+++ src/ped/src/ped_client.cpp
@@ -114,7 +114,29 @@ int main(int argc, char** argv){
 	pub_goal.publish(goal);
 	
 	
+	/// il momentaneo problema è che non so come fare per aspettare che il robot sia arrivato al goal, poichè quando provo
+	/// ad usare funzioni come waitForMessage su un topic quello che succede è che è come se non venissero chiamate le
+	/// funzioni di callback. allo stesso modo anche usando un ciclo while che aspetta fino a quando una variabile non 
+	/// viene modificata non va poichè anche qui non vengon visualizzate le print presneti nella funzione di callback
+	/// e quindi non viene eseguita tale funzione. non so come risovere questo problema devo chiedere al prof
+	
 	ros::spin();
 	return 0;
 }
 
+
+///l'idea che mi era venuta in mente era quella di:
+/// 1. chiedere all'utente le posizioni da far raggiungere al robot
+/// 2. aspettare che il robot arrivi al punto stabilito
+/// 3. aspettare che si riceva un messaggio (per segnalare che il pacco è stato messo)
+/// 4. chiedere all'utente le coordinate dove il robot deve andare per spedire il pacco
+/// 5. aspettare fino a quando il robot non raggiunge le coordinate inserite precedentemente
+/// 6. aspettare che il pacco venga preso
+/// 7.ripetere l'azione
+
+
+//pre ora il problema principale sta nella realizzazione del punto 2. poichè non so come far aspettare un nodo. so farlo con le 
+//so farlo usanod le actionlib ma non so farlo usando solamente i publisher e subscriber. 
+
+//finchè non risolvo il punto 2- non posso andare avanti e fare i punti seguenti, poichè il punto 5 è identico al punto 2 e 
+//il punto 7 implicherà cambiare delle cose ma si vedrà in seguito
